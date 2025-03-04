0a9f55ad5d33d77d8d121c90abeaa444100ddd9d
AntonAnderss <ande15andersson@gmail.com>
2025-03-04 18:19:26 +0100

    Made basic 3rd Person Camera

--- Grupp2Game/Assets/Scripts/Player/PlayerLook.cs
+++ Grupp2Game/Assets/Scripts/Player/PlayerLook.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
+//Anton Andersson
 public class PlayerLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+
+    [Header("MouseInputs")]
+    [SerializeField] Vector2 mouseVector;
+    [SerializeField] float sensetivity;
+
+    [Header("Rotation")]
+    [SerializeField] float horizontalRotation;
+    [SerializeField] float verticalRotation; //Isn't used
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetInputs();  
+        CalculateRotation();
+        ApplyRotation();
+    }
+
+    void GetInputs()
+    {
+        mouseVector = new Vector2(Input.GetAxisRaw("Mouse X") * sensetivity, Input.GetAxisRaw("Mouse Y") * sensetivity);
+    }
+
+    void CalculateRotation()
+    {
+        horizontalRotation += mouseVector.x;
+    }
+
+    void ApplyRotation()
+    {
+        transform.rotation = Quaternion.Euler(0, horizontalRotation, 0);
     }
 }
