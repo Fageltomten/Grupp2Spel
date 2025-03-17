00fa205a96348689ee5fd4c918d6bd83bcbd8ccf
Etmotsnatas <myrsol@gmail.com>
2025-03-17 14:16:51 +0100

    Breaking Platforms

--- Grupp2Game/Assets/Scripts/Platforms/BreakingPlatform.cs
+++ Grupp2Game/Assets/Scripts/Platforms/BreakingPlatform.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class BreakingPlatform : Platform
@@ -5,9 +6,18 @@ public class BreakingPlatform : Platform
     //TODO
     //Platform that breaks after you jump on it
     //And then despawn again
+    //Make the platform shake or change color before it breaks
+    //Make it spawn again after some time
+    [SerializeField] private float breakingDelay = 3f;
+    [SerializeField] private float spawnDelay = 10f;
+    private float timer;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
     protected override void Awake()
     {
         SetInitialStartingPosition();
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
     }
 
     protected override void FixedUpdate()
@@ -43,10 +53,32 @@ public class BreakingPlatform : Platform
     protected override void OnTriggerEnter(Collider other)
     {
         other.transform.SetParent(transform);
+        StartCoroutine(BreakPlatform());
 
     }
     protected override void OnTriggerExit(Collider other)
     {
         other.transform.SetParent(null);
+    }
+    private IEnumerator BreakPlatform()
+    {
+        yield return new WaitForSeconds(breakingDelay);
+        foreach (Transform child in transform)
+        {
+            child.SetParent(null);
+        }
+        gameObject.SetActive(false);
+        StartCoroutine(RespawnAfterDelay());
+    }
+    private IEnumerator RespawnAfterDelay()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        RespawnPlatform();
+    }
+    private void RespawnPlatform()
+    {
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+        gameObject.SetActive(true);
     }
 }
