8a976c1905217e144626a4e7abdf170d53cf1764
Etmotsnatas <myrsol@gmail.com>
2025-03-18 09:23:11 +0100

    PlatformSpawner

---
+++ Grupp2Game/Assets/PlatformSpawn.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlatformSpawn : MonoBehaviour
+{
+
+    [SerializeField] GameObject platform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
