d096e04b1f6084cf50c1f2f6d269db1f6368d237
Fageltomten <caslund2005@gmail.com>
2025-04-02 18:37:47 +0200

    SettingsMenu UI

---
+++ Grupp2Game/Assets/Scripts/Settings/SettingsMenuController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+
+// Author: Carl Åslund
+public class SettingsController : MonoBehaviour
+{
+
+    [SerializeField] GameObject aside;
+    [SerializeField] GameObject graphicsScreen;
+    [SerializeField] GameObject audioScreen;
+    [SerializeField] GameObject gameplayScreen;
+    [SerializeField] GameObject mainMenu;
+
+    public void EnterSettingsMenu()
+    {
+        mainMenu.SetActive(false);
+        aside.SetActive(true);
+        DeactivateScreens();
+    }
+
+    public void ExitSettingsMenu() 
+    {
+        DeactivateScreens();
+        aside.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+
+    public void GoToGraphics()
+    {
+        DeactivateScreens();
+        graphicsScreen.SetActive(true);
+    }
+
+    public void GoToAudio()
+    {
+        DeactivateScreens();
+        audioScreen.SetActive(true);
+    }
+
+    public void GoToGameplay()
+    {
+        DeactivateScreens();
+        gameplayScreen.SetActive(true);
+    }
+
+
+    private void DeactivateScreens()
+    {
+        graphicsScreen.SetActive(false);
+        audioScreen.SetActive(false);
+        gameplayScreen.SetActive(false);
+    }
+
+}
